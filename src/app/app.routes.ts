defac96fb3222b25fed4dab01bfc810a614d7dba
Markandeya <vasalpha@gmail.com>
2026-03-07 09:10:56 +0530

    perf: Implement lazy loading for routes and optimize asset loading for improved performance, SEO, and accessibility.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,24 @@
 import { Routes } from '@angular/router';
-import { MaashreePage } from './pages/maashree-page/maashree-page';
 import { LandingPage } from './pages/landing-page/landing-page';
-import { ShreejaAshramComponent } from './pages/shreeja-ashram/shreeja-ashram.component';
-import { DonatePageComponent } from './pages/donate-page/donate-page.component';
-import { NewsletterJan2025Component } from './pages/newsletters/newsletter-jan-2025/newsletter-jan-2025';
-import { NewsletterJul2025Component } from './pages/newsletters/newsletter-jul-2025/newsletter-jul-2025';
-import { NewsletterJanJun2025Component } from './pages/newsletters/newsletter-jan-jun-2025/newsletter-jan-jun-2025';
-import { NewsletterJanApr2024 } from './pages/newsletters/newsletter-jan-apr-2024/newsletter-jan-apr-2024';
-import { NewsletterMayAug2024 } from './pages/newsletters/newsletter-may-aug-2024/newsletter-may-aug-2024';
-import { NewsletterOctDec2024 } from './pages/newsletters/newsletter-oct-dec-2024/newsletter-oct-dec-2024';
-import { NewslettersOverviewComponent } from './pages/newsletters/newsletters-overview/newsletters-overview';
-import { NotFoundComponent } from './pages/not-found/not-found';
-import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy';
-import { StandForAnimalsComponent } from './pages/karmayog/stand-for-animals-page/stand-for-animals.component';
-import { BhavasanaComponent } from './pages/karmayog/bhavasana-page/bhavasana.component';
-import { ShreeGuruVidyalayaComponent } from './pages/karmayog/shree-guru-vidyalaya-page/shree-guru-vidyalaya.component';
-import { IkshanaaComponent } from './pages/ikshanaa/ikshanaa.component';
-import { WipComponent } from './pages/wip/wip.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPage },
-    { path: 'maashree', component: MaashreePage },
-    { path: 'shreeja-ashram', component: ShreejaAshramComponent },
-    { path: 'donate', component: DonatePageComponent },
-    { path: 'newsletters', component: NewslettersOverviewComponent },
-    { path: 'newsletters/jan-2025', component: NewsletterJan2025Component },
-    { path: 'newsletters/jan-jun-2025', component: NewsletterJanJun2025Component },
-    { path: 'newsletters/jul-2025', component: NewsletterJul2025Component },
-    { path: 'newsletters/jan-apr-2024', component: NewsletterJanApr2024 },
-    { path: 'newsletters/may-aug-2024', component: NewsletterMayAug2024 },
-    { path: 'newsletters/oct-dec-2024', component: NewsletterOctDec2024 },
-    { path: 'karmayog/stand-for-animals', component: StandForAnimalsComponent },
-    { path: 'karmayog/bhavasana', component: BhavasanaComponent },
-    { path: 'karmayog/shree-guru-vidyalaya', component: ShreeGuruVidyalayaComponent },
-    { path: 'ikshanaa', component: IkshanaaComponent },
-    { path: 'privacy-policy', component: PrivacyPolicyComponent },
-    { path: 'wip', component: WipComponent },
-    { path: '404', component: NotFoundComponent },
-    { path: '**', component: NotFoundComponent }
+    { path: 'maashree', loadComponent: () => import('./pages/maashree-page/maashree-page').then(m => m.MaashreePage) },
+    { path: 'shreeja-ashram', loadComponent: () => import('./pages/shreeja-ashram/shreeja-ashram.component').then(m => m.ShreejaAshramComponent) },
+    { path: 'donate', loadComponent: () => import('./pages/donate-page/donate-page.component').then(m => m.DonatePageComponent) },
+    { path: 'newsletters', loadComponent: () => import('./pages/newsletters/newsletters-overview/newsletters-overview').then(m => m.NewslettersOverviewComponent) },
+    { path: 'newsletters/jan-2025', loadComponent: () => import('./pages/newsletters/newsletter-jan-2025/newsletter-jan-2025').then(m => m.NewsletterJan2025Component) },
+    { path: 'newsletters/jan-jun-2025', loadComponent: () => import('./pages/newsletters/newsletter-jan-jun-2025/newsletter-jan-jun-2025').then(m => m.NewsletterJanJun2025Component) },
+    { path: 'newsletters/jul-2025', loadComponent: () => import('./pages/newsletters/newsletter-jul-2025/newsletter-jul-2025').then(m => m.NewsletterJul2025Component) },
+    { path: 'newsletters/jan-apr-2024', loadComponent: () => import('./pages/newsletters/newsletter-jan-apr-2024/newsletter-jan-apr-2024').then(m => m.NewsletterJanApr2024) },
+    { path: 'newsletters/may-aug-2024', loadComponent: () => import('./pages/newsletters/newsletter-may-aug-2024/newsletter-may-aug-2024').then(m => m.NewsletterMayAug2024) },
+    { path: 'newsletters/oct-dec-2024', loadComponent: () => import('./pages/newsletters/newsletter-oct-dec-2024/newsletter-oct-dec-2024').then(m => m.NewsletterOctDec2024) },
+    { path: 'karmayog/stand-for-animals', loadComponent: () => import('./pages/karmayog/stand-for-animals-page/stand-for-animals.component').then(m => m.StandForAnimalsComponent) },
+    { path: 'karmayog/bhavasana', loadComponent: () => import('./pages/karmayog/bhavasana-page/bhavasana.component').then(m => m.BhavasanaComponent) },
+    { path: 'karmayog/shree-guru-vidyalaya', loadComponent: () => import('./pages/karmayog/shree-guru-vidyalaya-page/shree-guru-vidyalaya.component').then(m => m.ShreeGuruVidyalayaComponent) },
+    { path: 'ikshanaa', loadComponent: () => import('./pages/ikshanaa/ikshanaa.component').then(m => m.IkshanaaComponent) },
+    { path: 'privacy-policy', loadComponent: () => import('./pages/privacy-policy/privacy-policy').then(m => m.PrivacyPolicyComponent) },
+    { path: 'wip', loadComponent: () => import('./pages/wip/wip.component').then(m => m.WipComponent) },
+    { path: '404', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFoundComponent) },
+    { path: '**', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFoundComponent) }
 ];
