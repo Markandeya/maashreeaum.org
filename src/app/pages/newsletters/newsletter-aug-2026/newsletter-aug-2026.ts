b40fe5d3a9e70f1e4da3b569873e2fa00dbc4d03
Markandeya <vasalpha@gmail.com>
2026-09-21 21:06:43 +0530

    feat: add Aug 2026 newsletter (Vol. 11)

---
+++ src/app/pages/newsletters/newsletter-aug-2026/newsletter-aug-2026.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NewsletterLayoutComponent } from '../newsletter-layout/newsletter-layout';
+
+@Component({
+    selector: 'app-newsletter-aug-2026',
+    imports: [NewsletterLayoutComponent],
+    templateUrl: './newsletter-aug-2026.html',
+    styleUrl: './newsletter-aug-2026.scss',
+})
+export class NewsletterAug2026Component { }
